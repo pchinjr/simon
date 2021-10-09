adb3803af281c6dccfe7e1138784ea6572f9f4bc
Paul Chin Jr <pchinjr@gmail.com>
2021-10-09 15:42:36 -0400

    integrated vibe

--- main.js
+++ main.js
@@ -115,3 +115,86 @@ titleContainer.addEventListener('click', event => {
 
   if (tile) handleClick(tile)
 })
+
+
+async function runDeviceControlExample() {
+  // Instantiate our wasm module. This only needs to be done once. If you did it
+  // elsewhere, ignore this.
+  await Buttplug.buttplugInit();
+
+  // Usual embedded connector setup.
+  const connector = new Buttplug.ButtplugEmbeddedConnectorOptions();
+  const client = new Buttplug.ButtplugClient("Device Control Example");
+  await client.connect(connector);
+
+  // Set up our DeviceAdded/DeviceRemoved event handlers before connecting. If
+  // devices are already held to the server when we connect to it, we'll get
+  // "deviceadded" events on successful connect.
+  client.addListener("deviceadded", async (device) => {
+    console.log(`Device Connected: ${device.Name}`);
+    console.log("Client currently knows about these devices:");
+    client.Devices.forEach((device) => console.log(`- ${device.Name}`));
+
+    // In Javascript, allowedMessages is a map, so we'll need to iterate its
+    // properties.
+
+    console.log("Sending commands");
+
+    // If we aren't working with a toy that vibrates, just return at this point.
+    if (!device.messageAttributes(Buttplug.ButtplugDeviceMessageType.VibrateCmd)) {
+      return;
+    }
+
+    // Now that we know the message types for our connected device, and that our
+    // device handles vibration, we can send a message over!
+    //
+    // There's a couple of ways to send this message.
+
+    // We can use the convenience functions on ButtplugClientDevice to
+    // send the message. This version sets all of the motors on a
+    // vibrating device to the same speed.
+    let vibeButton = document.getElementById("vibe")
+    vibeButton.onclick = function doSomething() {
+      console.log('vibe clicked')
+      device.vibrate(1.0)
+    }
+
+    let stopButton = document.getElementById("stop")
+    stopButton.onclick = function doSomething() {
+      console.log('stop clicked')
+      device.stop()
+    }
+
+    // If we wanted to just set one motor on and the other off, we could
+    // try this version that uses an array. It'll throw an exception if
+    // the array isn't the same size as the number of motors available as
+    // denoted by FeatureCount, though.
+    //
+    // You can get the vibrator count using the following code, though we
+    // know it's 2 so we don't really have to use it.
+    //
+    // This vibrateType variable is just used to keep us under 80
+    // characters for the dev guide, so don't feel that you have to reassign
+    // types like this. I'm just trying to make it so you don't have to
+    // horizontally scroll in the manual. :)
+
+    /*
+    var vibratorCount =
+      device.AllowedMessages[vibrateType].FeatureCount;
+    await testClientDevice.SendVibrateCmd(new [] { 1.0, 0.0 });
+    */
+  });
+  client
+    .addListener("deviceremoved", (device) => console.log(`Device Removed: ${device.Name}`));
+
+  // Now that everything is set up, we can scan.
+  let connectButton = document.getElementById("connect-toy")
+  connectButton.onclick = function doSomething() {
+    console.log('connect clicked')
+    client.startScanning();
+  }
+
+
+};
+
+runDeviceControlExample()
